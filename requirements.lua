b08376039656b285c59f76d5ebe0bb1e570a951e
hyperwrld <pubglife2121@gmail.com>
2021-10-29 20:56:51 -0500

    Dependecnys

---
+++ requirements.lua
@@ -0,0 +1,17 @@
+ k9 dependencys:
+
+ context script
+ keymapping script
+ target script
+
+ 
+ 
+ Make your own item in the shared list
+ 
+--inventory: function.lua
+  if (itemid == "k9")  then
+        TriggerEvent("K9:Create")
+    end
+
+
+the keyymapping and other things that you have to do is because I dont know/have access to your scripts, sorry
